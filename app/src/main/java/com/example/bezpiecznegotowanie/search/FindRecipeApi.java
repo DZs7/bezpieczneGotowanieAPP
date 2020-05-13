a1066e011e40cc801ebaad66b259750619eb1474
piotr <piosoboci@gmail.com>
2020-05-13 19:37:49 +0200

    Recipe creator refactor

---
+++ app/src/main/java/com/example/bezpiecznegotowanie/search/FindRecipeApi.java
@@ -0,0 +1,87 @@
+package com.example.bezpiecznegotowanie.search;
+
+import android.net.Uri;
+
+import com.example.bezpiecznegotowanie.Recipe;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class FindRecipeApi {
+
+
+    private String mQueryIngredients = "";
+    private String mQueryRecipeCount;
+    private String mQueryPreparationTime;
+    private String mErrorMessage = "";
+    private String mBuiltURL;
+
+    private final static String BASE_URI = "https://api.edamam.com/search?";
+    private final static String PARAM_Q = "q";
+    private final static String PARAM_TO = "to";
+    private final static String PARAM_TIME = "time";
+    private final static String PARAM_APP_ID = "app_id";
+    private final static String PARAM_APP_KEY = "app_key";
+    private final static String APP_ID = "d5494739";
+    private final static String APP_KEY = "9c1fb9f937e7decb9d096753ea325235";
+
+
+    FindRecipeApi(List<String> ingredients, int recipeCount, int preparationTime) {
+        for (String ingredient : ingredients)
+            mQueryIngredients += ingredient + ",";
+        mQueryRecipeCount = String.valueOf(recipeCount);
+        mQueryPreparationTime = String.valueOf(preparationTime);
+    }
+
+    private String buildSearchURL() {
+        Uri searchURL = Uri.parse(BASE_URI).buildUpon()
+                .appendQueryParameter(PARAM_Q, mQueryIngredients)
+                .appendQueryParameter(PARAM_TO, mQueryRecipeCount)
+                .appendQueryParameter(PARAM_TIME, mQueryPreparationTime)
+                .appendQueryParameter(PARAM_APP_ID, APP_ID)
+                .appendQueryParameter(PARAM_APP_KEY, APP_KEY)
+                .build();
+        mBuiltURL = searchURL.toString();
+        return searchURL.toString();
+    }
+
+    private String getResultJSON(){
+        mErrorMessage = "";
+        StringBuilder resultJSONBuilder = new StringBuilder();
+        try {
+            URL url = new URL(this.buildSearchURL());
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.connect();
+            BufferedReader responseBodyReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            if(urlConnection.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+                String line;
+                while ((line = responseBodyReader.readLine()) != null)
+                    resultJSONBuilder.append(line).append("\n");
+            }
+            else{
+                mErrorMessage = urlConnection.getResponseCode() + urlConnection.getResponseMessage();
+            }
+            urlConnection.disconnect();
+            responseBodyReader.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return resultJSONBuilder.toString();
+    }
+
+    List<Recipe> getResult(){
+        return JSONParser.parseResultJSON(getResultJSON());
+    }
+
+    String getBuiltURL() {
+        return mBuiltURL;
+    }
+
+    String getErrorMessage() {
+        return mErrorMessage;
+    }
+}
---
+++ app/src/main/java/com/example/bezpiecznegotowanie/search/JSONParser.java
@@ -0,0 +1,45 @@
+package com.example.bezpiecznegotowanie.search;
+
+import com.example.bezpiecznegotowanie.Recipe;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONParser {
+
+
+    private final static String JSON_KEY_RECIPE_ARRAY = "hits";
+    private final static String JSON_KEY_RECIPE_OBJECT = "recipe";
+    private final static String JSON_KEY_LABEL = "label";
+    private final static String JSON_KEY_IMAGE_URL = "image";
+    private final static String JSON_KEY_SOURCE_URL = "url";
+    private final static String JSON_KEY_INGREDIENTS_ARRAY = "ingredientLines";
+
+    static List<Recipe> parseResultJSON(String resultJSON){
+        List<Recipe> recipeList = new ArrayList<>();
+        try {
+            JSONObject jsonObject = new JSONObject(resultJSON);
+            JSONArray recipeArray = jsonObject.getJSONArray(JSON_KEY_RECIPE_ARRAY);
+            for(int i = 0; i < recipeArray.length(); i++){
+                JSONObject recipeJSON = recipeArray.getJSONObject(i).getJSONObject(JSON_KEY_RECIPE_OBJECT);
+                String label = recipeJSON.getString(JSON_KEY_LABEL);
+                String imageURL = recipeJSON.getString(JSON_KEY_IMAGE_URL);
+                String sourceURL = recipeJSON.getString(JSON_KEY_SOURCE_URL);
+                JSONArray ingredientsArray = recipeJSON.getJSONArray(JSON_KEY_INGREDIENTS_ARRAY);
+                List<String> ingredientsList = new ArrayList<>();
+                for(int j = 0; j < ingredientsArray.length(); j++)
+                    ingredientsList.add(ingredientsArray.getString(j));
+                Recipe recipe = new Recipe(label, imageURL, sourceURL, ingredientsList);
+                recipeList.add(recipe);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return recipeList;
+    }
+}
+
