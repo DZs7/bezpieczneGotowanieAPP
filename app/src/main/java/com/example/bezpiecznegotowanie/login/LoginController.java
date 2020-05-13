4194ef066142834477c5fe0a721dd6cea81ab497
mariusztota <58478622+mariusztota@users.noreply.github.com>
2020-05-13 21:34:32 +0200

    Logowanie użytkowników

--- app/src/main/java/com/example/bezpiecznegotowanie/login/LoginController.java
+++ app/src/main/java/com/example/bezpiecznegotowanie/login/LoginController.java
@@ -1,4 +1,87 @@
 package com.example.bezpiecznegotowanie.login;
 
-public class LoginController {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.bezpiecznegotowanie.R;
+import com.example.bezpiecznegotowanie.search.FindAction;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginController extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private EditText mEmail;
+    private EditText mPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mEmail = findViewById(R.id.email_input);
+        mPassword = findViewById(R.id.password_input);
+        mEmail.setText("");
+        mPassword.setText("");
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void startAppWithoutSigning(View v){
+        Intent intent = new Intent(this, FindAction.class);
+        startActivity(intent);
+    }
+
+    public void register(View v){
+
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+        if(email.isEmpty() || password.isEmpty()) {
+            displayToast(getString(R.string.missing_email_password_message));
+            return;
+        }
+        mFirebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, authTaskResult -> {
+                    if(authTaskResult.isSuccessful()) {
+                        displayToast("Your account has been successfully registered\nPlease sign in to proceed");
+                    }
+                    else{
+                        displayToast("Registration Failed: " + authTaskResult.getException().getMessage());
+                    }
+
+                });
+
+    }
+
+    public void signIn(View v){
+
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+        if(email.isEmpty() || password.isEmpty()) {
+            displayToast(getString(R.string.missing_email_password_message));
+            return;
+        }
+
+        mFirebaseAuth.signInWithEmailAndPassword(email, password).
+                addOnCompleteListener(this, authTaskResult -> {
+                    if(authTaskResult.isSuccessful()){
+                        Intent intent = new Intent(LoginController.this, FindAction.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        displayToast(authTaskResult.getException().getMessage());
+                    }
+
+                });
+
+
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
